27148d2f288932f4b41c046bad3fa4ce23476e2b
Plutocracy <luckydogbey@gmail.com>
2021-10-29 13:42:07 +0000

    pushing to live

---
+++ website/forms.py
@@ -0,0 +1,8 @@
+from .models import *
+from django.forms import ModelForm, fields
+
+
+class ContactUsForm(ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = ['name', 'email', 'phone_number', 'description']
--- website/models.py
+++ website/models.py
@@ -11,3 +11,16 @@ class Banner(models.Model):
     
     def __str__(self):
         return self.name
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=500)
+    email = models.CharField(max_length=500, blank=True, null=True, default='')
+    phone_number = models.CharField(max_length=50)
+    description = models.TextField()
+    is_contacted = models.BooleanField(default=True)
+    time_contacted = models.DateTimeField(default='', blank=True, null=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.name
+    
